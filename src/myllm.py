8fdd476fa5e9ac981d0f19a7cc0d46b00007ef24
Farshad Nassiri <farshadnassiri@gmail.com>
2025-11-02 13:21:01 +0330

    adding llm module to project

---
+++ src/myllm.py
@@ -0,0 +1,12 @@
+from google import genai
+
+def chat_wit_llm(prompt):
+    client = genai.Client()
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=prompt
+    )
+
+    print(response.text)
+
+
